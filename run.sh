af1226f73ada54e0648e9ea9b62146f3044bee41
Ronen Ben Yaakov <106447438+ronenBenYaakov@users.noreply.github.com>
2026-06-30 04:18:36 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,18 @@ mkdir -p build
 cd build
 cmake ..
 make
+
 python ../app.py &
-sudo ./deadlock-interceptor $! group
-ps -p $!
+PID=$!
+
+echo "[*] Started Python PID: $PID"
+
+sleep 1
+
+echo "[*] Thread list (/proc/$PID/task):"
+ls /proc/$PID/task
+
+echo "[*] Thread count:"
+ls /proc/$PID/task | wc -l
+
+sudo ./deadlock-interceptor $PID group
